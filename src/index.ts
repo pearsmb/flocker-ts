b2ee47cceffae01d0c33646f036a2ebd48ccf6de
pearsb <pearsonmben@outlook.com>
2022-09-04 12:30:14 +0100

    Removed Gameboard + Game classes. Added animated canvas window.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
 
 
+window.onload = init;
+
+const canvas = createGameCanvas();
+const context = canvas.getContext('2d');
 
 
+function init(){
+
+    document.body.appendChild(canvas);
+    
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+function gameLoop(timeStamp : number){
+    draw();
+
+    // Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+}
+
+function draw(){
+    let randomColor = Math.random() > 0.5? '#ff8080' : '#0099b0';
+
+    if(context == null){
+        return;
+    }
+
+    context.fillStyle = randomColor;
+    context.fillRect(100, 50, 200, 175);
+}
+
+
+
+
+
+function createGameCanvas() : HTMLCanvasElement {
+
+    let canvas = document.createElement("canvas");
+
+    canvas.width = 400;
+    canvas.height = 300;
+
+    return canvas;
+
+}
 
